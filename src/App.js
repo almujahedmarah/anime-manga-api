446ac421bda8b561b849f1c0080e1e99204512e4
عبدالله الصبي <Abdullahb.alsabi@gmail.com>
2021-10-27 16:21:10 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
-import Card from "./component/Card";
-import GetData from "./component/GetData";
-function hundleAnimeBtn() {
-  return <GetData name="Anime" />;
-}
-function hundleMangaBtn() {
-  return <GetData name="Manga" />;
-}
 function App() {
+  let [resType, setResType] = useState("anime");
+  let [items, setItems] = useState([]);
+
+  useEffect(() => {
+    Axios.get("https://kitsu.io/api/edge//trending/" + resType)
+      .then((response) => {
+        setItems(response.data.data);
+      })
+      .catch((error) => console.log(error));
+  }, [resType]);
+
+  function hundleAnimeBtn() {
+    setResType("anime");
+    console.log(resType);
+    console.log(items);
+  }
+  function hundleMangaBtn() {
+    setResType("manga");
+    console.log(resType);
+    console.log(items);
+  }
   return (
     <>
       <div className="btnMain">
@@ -20,25 +35,23 @@ function App() {
         </button>
       </div>
       <div className="cardMain">
-        <Card
-          name="anime"
-          img="https://png.pngtree.com/png-vector/20191129/ourmid/pngtree-office-checklist-icon-business-checklist-survey-test-icon-png-image_2047566.jpg"
-        />
-        <Card
-          name="anime"
-          img="https://png.pngtree.com/png-vector/20191129/ourmid/pngtree-office-checklist-icon-business-checklist-survey-test-icon-png-image_2047566.jpg"
-        />
-        <Card
-          name="anime"
-          img="https://png.pngtree.com/png-vector/20191129/ourmid/pngtree-office-checklist-icon-business-checklist-survey-test-icon-png-image_2047566.jpg"
-        />
-        <Card
-          name="anime"
-          img="https://png.pngtree.com/png-vector/20191129/ourmid/pngtree-office-checklist-icon-business-checklist-survey-test-icon-png-image_2047566.jpg"
-        />
+        {items.map((item) => {
+          if (item.attributes.titles.en == null) {
+            return null;
+          } else
+            return (
+              <div className="card">
+                <h3>{item.attributes.titles.en}</h3>
+                <img
+                  className="img-fluid"
+                  src={item.attributes.posterImage.large}
+                  alt="img"
+                ></img>
+              </div>
+            );
+        })}
       </div>
     </>
   );
 }
-
 export default App;
--- src/component/GetData.js
+++ src/component/GetData.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
+import Axios from "axios";
 
 export default function GetData(props) {
+  const [type, setType] = useState();
+  function getData() {
+    Axios.get("https://kitsu.io/api/edge//trending/" + props.name)
+      .then((response) => {
+        setType(response);
+      })
+      .catch((error) => console.log(error));
+  }
   console.log("iam in getdata???" + props.name);
   return <div>GetData {props.name}</div>;
 }
